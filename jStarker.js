dea795e330dbeff17febeb646c99d044ee673f07
hamwithglasses <hello@kegh.am>
2014-06-13 23:01:50 -0700

    UI
    Basic UI added to better visualize what keys you’re actually pressing.

--- jStarker.js
+++ jStarker.js
@@ -12,7 +12,7 @@
 	else
 		context = new webkitAudioContext();
 
-	var jStarker = function() {
+	var jStarker = function(draw) {
 			var keysPressed = {}, active = {},
 				isTenor = false, masterVolume = 1;
 
@@ -26,6 +26,14 @@
 				186:'G3',65:'G#3',83:'A3',68:'A#3',70:'B3',71:'C4',72:'C#4',74:'D4',75:'D#4',76:'E4',
 				//C String
 				191:'C3',90:'C#3',88:'D3',67:'D#3',86:'E3',66:'F3',78:'F#3',77:'G3',188:'G#3',190:'A3'
+			};
+
+			//Note hierarchy for each string
+			var noteOrder = {
+				48: [57, 56, 55, 54, 53, 52, 51, 50, 49, 48],
+				80: [79, 73, 85, 89, 84, 82, 69, 87, 81, 80],
+				186: [76, 75, 74, 72, 71, 70, 68, 83, 65, 186],
+				191: [190, 188, 77, 78, 66, 86, 67, 88, 90, 191]
 			};
 
 			//Checks if FF: http://stackoverflow.com/questions/7000190/detect-all-firefox-versions-in-js
@@ -60,14 +68,6 @@
 			//Takes a keyboard keycode and returns the highest key pressed on teh string
 			//Keycode being passed should be a string key: 0 p ; /
 			var getCelloNote = function(keyCode) {
-				//Note hierarchy for each string
-				var noteOrder = {
-					48: [57, 56, 55, 54, 53, 52, 51, 50, 49, 48],
-					80: [79, 73, 85, 89, 84, 82, 69, 87, 81, 80],
-					186: [76, 75, 74, 72, 71, 70, 68, 83, 65, 186],
-					191: [190, 188, 77, 78, 66, 86, 67, 88, 90, 191]
-				};
-
 				//Makes sure key pressed is a string key
 				var string = noteOrder[keyCode];
 				if (string) {
@@ -85,6 +85,8 @@
 
 				//Checks if we're dealing with a supported note key
 				if(keyToString[keyCode]){
+					if(draw && document.getElementById('key' + keyCode))
+						document.getElementById('key' + keyCode).className = 'pressed';
 
 					//Makes sure the key is not pressed and the string is not playing
 					if (!active[keyCode] && !keysPressed[keyCode]) {
@@ -107,14 +109,19 @@
 			var keyboardUp = function(key) {
 				var keyCode = keyboardSupport(key.keyCode);
 
-				//Only stops note if string key is lifted
-				if (keysPressed[191] || keysPressed[80] || keysPressed[48] || keysPressed[186]) {
-					if (active[keyCode]) {
-						active[keyCode].stop(0);
-						delete active[keyCode];
+				if(keyToString[keyCode]){
+					if(draw && document.getElementById('key' + keyCode))
+						document.getElementById('key' + keyCode).className = '';
+
+					//Only stops note if string key is lifted
+					if (keysPressed[191] || keysPressed[80] || keysPressed[48] || keysPressed[186]) {
+						if (active[keyCode]) {
+							active[keyCode].stop(0);
+							delete active[keyCode];
+						}
 					}
+					delete keysPressed[keyCode];
 				}
-				delete keysPressed[keyCode];
 			};
 
 			//Add sound generation/playing here
@@ -130,6 +137,55 @@
 
 				osc.start(0);
 			};
+
+			var drawString = function(s, c){
+				var fingering = ['0', '-', '1', '2', '3', '4', '+'];
+				var li = document.createElement('li');
+
+				var notes = noteOrder[s];
+
+				var stringUL = document.createElement('ul');
+				stringUL.id = c + '-string';
+				stringUL.style.listStyle = "none";
+				stringUL.style.padding = 0;
+
+				for(var i = 1; i < 7 + 1; i++){
+
+					var key = document.createElement('li');
+					key.style.cursor = 'pointer';
+
+					if(i == 7){
+						key.innerHTML = fingering[0];
+						key.id = 'key' + notes[notes.length - 1];
+					}else{
+						key.innerHTML = fingering[i];
+						key.id = 'key' + notes[notes.length - 1 - i];
+					}
+					stringUL.appendChild(key);
+				}
+				return li.appendChild(stringUL);
+			}
+
+			//Draws base keyboard UI
+			var drawKeyboard = function(){
+				var ul = document.createElement('ul');
+				ul.id = 'jStarker';
+				ul.style.listStyle = 'none';
+				ul.style.padding = 0;
+				ul.style['-webkit-user-select'] = 'none';
+
+				//Adding strings
+				ul.appendChild(drawString(191, 'c'));
+				ul.appendChild(drawString(186, 'g'));
+				ul.appendChild(drawString(80, 'd'));
+				ul.appendChild(drawString(48, 'a'));
+
+				document.body.appendChild(ul);
+			}
+
+			if(draw !== undefined && draw == true)
+				drawKeyboard();
+
 
 			window.onkeydown = keyboardDown;
 			window.onkeyup = keyboardUp;
